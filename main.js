c864e518aea29a6b2fec9bff029acf87b4da1fa2
acrule <acrule@gmail.com>
2017-04-06 12:38:38 -0700

    refactor for readability

--- main.js
+++ main.js
@@ -12,20 +12,13 @@ define([
     events
 ){
 
-    // TODO Find out how to remove this error message
-    // "accessing "actions" on the global IPython/Jupyter is not recommended. Pass it to your objects contructors at creation time"
+    // Throws console warning:
+    // "accessing "actions" on the global IPython/Jupyter is not recommended.
+    // Pass it to your objects contructors at creation time"
     var ActionHandler = Jupyter.actions;
 
-    // Lists of actions to track. For all available actions see:
+    // List of actions to track. For all available actions see:
     // https://github.com/jupyter/notebook/blob/master/notebook/static/notebook/js/actions.js
-    var run_actions = [
-        'run-cell',
-        'run-cell-and-select-next',
-        'run-cell-and-insert-below',
-        'run-all-cells',
-        'run-all-cells-above',
-        'run-all-cells-below'
-    ]
     var actions_to_intercept = [
         // execute cells
         'run-cell',
@@ -58,15 +51,21 @@ define([
         'change-cell-to-code',
         'change-cell-to-raw',
         // change display of cell output
-        //'toggle-cell-output-collapsed',
-        //'toggle-cell-output-scrolled',
-        //'clear-cell-output',
-        //'restart-kernel-and-clear-output',
-        //'confirm-restart-kernel-and-clear-output',
+        'clear-cell-output',
+        'restart-kernel-and-clear-output',
+        'toggle-cell-output-collapsed',
+        'toggle-cell-output-scrolled',
+        // 'confirm-restart-kernel-and-clear-output',
+
     ];
 
-    function sendData(t, actionName, selectedIndex, selectedIndices, mod, url){
-        /* Send data about the action to the Comet server extension*/
+    function sendData(nb, t, actionName, selectedIndex, selectedIndices){
+        /* Send data about the action to the Comet server extension */
+
+        var mod = nb.toJSON();
+        var notebookUrl =  nb.notebook_path;
+        var baseUrl = nb.base_url;
+        var url = utils.url_path_join(baseUrl, 'api/comet', notebookUrl);
 
         var d = JSON.stringify({
             time: t,
@@ -87,8 +86,7 @@ define([
         var response = utils.promising_ajax(url, settings);
     }
 
-    // TODO figure out how to make compatable with 4.0.0 notebook
-    function patch_actionHandler_call(){
+    function patchActionHandlerCall(){
         /* Inject code into the actionhandler to track desired actions */
 
         console.log('[Comet] patching ActionHandler.prototype.call');
@@ -99,41 +97,30 @@ define([
             var actionName = arguments[0].split(":")[1]; // remove 'jupter-notebook:' prefix
 
             if(actions_to_intercept.indexOf(actionName)>-1){
+
                 // get time, action name, and selected cell(s) before action applied
+                var nb = this.env.notebook
                 var t = Date.now();
-                var selectedIndex = this.env.notebook.get_selected_index();
-                var selectedIndices = this.env.notebook.get_selected_cells_indices();
-                var that = this
+                var selectedIndex = nb.get_selected_index();
+                var selectedIndices = nb.get_selected_cells_indices();
 
-                function record_output(evt, data){
-                    var mod = that.env.notebook.toJSON();
-                    var notebookUrl =  that.env.notebook.notebook_path;
-                    var baseUrl = that.env.notebook.base_url;
-                    var url = utils.url_path_join(baseUrl, 'api/comet', notebookUrl);
-
-                    // and send the data to the server extension for processing
-                    sendData(t, actionName, selectedIndex, selectedIndices, mod, url);
-
-                    events.off('finished_execute.CodeCell', record_output)
+                // if executing a Code cell, wait for it to execute before scraping the notebook
+                // notebook v. 5.0.0 added the finished_execute.CodeCell event
+                // that we may want to listen for instead of the kernel idleing
+                function sendDataAfterExecution(evt){
+                    sendData(nb, t, actionName, selectedIndex, selectedIndices);
+                    events.off('kernel_idle.Kernel', sendDataAfterExecution)
                 }
 
-                // need to wait for Code cells to execute before we can see the output
-                // v. 5.0.0 added the finished_execute.CodeCell event that we may want to listen for instead
-                if(run_actions.indexOf(actionName)>-1 && this.env.notebook.get_cell(selectedIndex).cell_type == "code"){
-                    events.on('kernel_idle.Kernel', record_output);                    
+                if(actionName.substring(0,3) == "run" && nb.get_cell(selectedIndex).cell_type == "code"){
+                    events.on('kernel_idle.Kernel', sendDataAfterExecution);
                     old_call.apply(this, arguments);
                 }
+                // otherwise just scrape the notebook right away
                 else{
-                    // let the notebook apply the action, and record the data
                     old_call.apply(this, arguments);
+                    sendData(nb, t, actionName, selectedIndex, selectedIndices);
 
-                    var mod = this.env.notebook.toJSON();
-                    var notebookUrl =  this.env.notebook.notebook_path;
-                    var baseUrl = this.env.notebook.base_url;
-                    var url = utils.url_path_join(baseUrl, 'api/comet', notebookUrl);
-
-                    // and send the data to the server extension for processing
-                    sendData(t, actionName, selectedIndex, selectedIndices, mod, url);
                 }
             }
             else{
@@ -142,13 +129,8 @@ define([
         }
     };
 
-    function record_output(t, actionName, selectedIndex, selectedIndices, nb){
-        // now get the modified notebook and its url
-
-    }
-
     function load_extension(){
-        patch_actionHandler_call();
+        patchActionHandlerCall();
     }
 
     return {
